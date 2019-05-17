28bc6f317de8acad967ca1c2d50bf7a90c911909
AntonVoronezh <pstut@mail.ru>
2019-05-17 09:27:48 +0300

    add AC in login

--- src/store/actions/login.js
+++ src/store/actions/login.js
@@ -5,6 +5,12 @@ export const ADD_NEW_PASSWORD_TEXT = 'ADD_NEW_PASSWORD_TEXT';
 export const CHANGE_REMEMBER = 'CHANGE_REMEMBER';
 export const VALIDATION_FORM = 'VALIDATION_FORM';
 
+export const addNewUsernameTextAC = text => ({ type: ADD_NEW_USERNAME_TEXT, payload: text });
+export const addNewPasswordTextAC = text => ({ type: ADD_NEW_PASSWORD_TEXT, payload: text });
+export const changeRememberAC = () => ({ type: CHANGE_REMEMBER });
+
+export const validationFormAC = () => ({ type: VALIDATION_FORM });
+
 
 	// dispatch(usersRequestAC());
 
