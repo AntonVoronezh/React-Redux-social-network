f5cb93ed2533e539cb8dc152c3bea5980898fb7d
AntonVoronezh <pstut@mail.ru>
2019-05-17 09:19:08 +0300

    refactoing axios

--- src/helpers/axios/axios.js
+++ src/helpers/axios/axios.js
@@ -6,6 +6,15 @@ export const getUsers = (page, count) => {
 	const api = '/users';
 	let payload = '';
 
+	if (page && count) {
+		payload = `/?page=${page}&count=${count}`;
+	} else if (page && !count) {
+		payload = `/?page=${page}`;
+	} else if (!page && count) {
+		payload = `/?count=${count}`;
+	} else {
+		payload = '';
+	}
 
 	return axios.get(`${url}${api}${payload}`);
 };
