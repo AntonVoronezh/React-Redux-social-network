a504156a015ea4f43e0b8ca2388511e6fe2f00e4
AntonVoronezh <pstut@mail.ru>
2019-05-17 13:10:15 +0300

    add users

---
+++ src/store/actions/users.js
@@ -0,0 +1,16 @@
+import { getUsers } from '../../helpers/axios/axios';
+
+export const USERS_REQUEST = 'USERS_REQUEST';
+export const USERS_SUCCESS = 'USERS_SUCCESS';
+export const USERS_FAILURE = 'USERS_FAILURE';
+
+export const FOLLOW = 'FOLLOW';
+export const UNFOLLOW = 'UNFOLLOW';
+
+export const GET_TOTAL_COUNT = 'GET_TOTAL_COUNT';
+export const SET_PAGE_NUMBER = 'SET_PAGE_NUMBER';
+
+
+
+
+
