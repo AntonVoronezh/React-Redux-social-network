3239ad15b4927d69e94c89b5441c80ea38799c5c
AntonVoronezh <pstut@mail.ru>
2019-05-21 11:11:05 +0300

    add Profile

---
+++ src/components/pages/Profile/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Segment } from 'semantic-ui-react';
+import Textarea from '../../elements/UI/Textarea/Textarea';
+import Button from '../../elements/UI/Button/Button';
+import Posts from '../../elements/Posts/Posts';
+
+const Profile = ({ posts, newMessageText, description, image, addPostCB, addNewMessageCB }) => {
+	return (
+
+	);
+};
+
+export default Profile;
+
+
