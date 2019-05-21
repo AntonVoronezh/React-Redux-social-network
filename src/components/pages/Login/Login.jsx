7e5d9d6028f96cf1901fe8cedccf9ce273e5abbe
AntonVoronezh <pstut@mail.ru>
2019-05-21 11:16:19 +0300

    add Login

---
+++ src/components/pages/Login/Login.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Form, Segment, Checkbox } from 'semantic-ui-react';
+
+const Login = ({
+	addNewUsernameTextCB,
+	username,
+	password,
+	addNewPasswordTextCB,
+	changeRememberCB,
+	remember,
+	validationFormCB,
+	loginCB
+}) => {
+
+
+
+
+	);
+};
+
+export default Login;
+
