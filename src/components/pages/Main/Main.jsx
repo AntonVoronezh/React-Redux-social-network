c89f1830d2783846120b06a7cf4e7a7171cf942a
AntonVoronezh <pstut@mail.ru>
2019-05-21 11:14:45 +0300

    add main

---
+++ src/components/pages/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Main = props => {
+	return <React.Fragment>Main ggg</React.Fragment>;
+};
+
+export default Main;
+
+Main.propTypes = {
+	usernameText: PropTypes.string,
+};
