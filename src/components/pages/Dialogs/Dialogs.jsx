a48f22a38cffdb98c1f6f525e870d7a91fa8b0cc
AntonVoronezh <pstut@mail.ru>
2019-05-22 08:50:40 +0300

    add Dialogs

---
+++ src/components/pages/Dialogs/Dialogs.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Segment } from 'semantic-ui-react';
+import classes from './Dialogs.module.css';
+import DialogsList from '../../elements/DialogsList/DialogsList';
+import Messages from '../../elements/Messages/Messages';
+import UsersMessagesForm from '../../elements/forms/UsersMessagesForm/UsersMessagesForm';
+
+const Dialogs = props => {
+	return (
+		<React.Fragment>
+			<div className={classes.dialogs}>
+	
+
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default Dialogs;
+
