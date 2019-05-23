6f60a4fe879c7eef57981c89e08340f00fc98812
AntonVoronezh <pstut@mail.ru>
2019-05-23 09:20:12 +0300

    add Fragment

--- src/components/elements/LeftMenu/LeftMenu.jsx
+++ src/components/elements/LeftMenu/LeftMenu.jsx
@@ -5,7 +5,15 @@ import { Menu } from 'semantic-ui-react';
 
 const LeftMenu = props => {
 	return (
-
+		<React.Fragment>
+			<Menu fluid vertical tabular>
+				<Menu.Item activeClassName="active" as={NavLink} exact to="/" content="Главная" />
+				<Menu.Item activeClassName="active" as={NavLink} to="/profile" content="Профайл" />
+				<Menu.Item activeClassName="active" as={NavLink} to="/dialogs" content="Сообщения" />
+				<Menu.Item activeClassName="active" as={NavLink} to="/users" content="Пользователи" />
+				<Menu.Item activeClassName="active" as={NavLink} to="/login" content="Логин" />
+			</Menu>
+		</React.Fragment>
 	);
 };
 
