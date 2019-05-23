27d23a2bed0d06d767823ab4c2e146af71e48e0b
AntonVoronezh <pstut@mail.ru>
2019-05-23 09:22:24 +0300

    add Messages

---
+++ src/components/elements/Messages/Messages.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { List } from 'semantic-ui-react';
+import Message from './Message/Message';
+
+
+
+export default Messages;
+
