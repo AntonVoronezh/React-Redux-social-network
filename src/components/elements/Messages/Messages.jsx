c1f41698ab4eb81b89ac77af7feaeb05c1829c7c
AntonVoronezh <pstut@mail.ru>
2019-05-23 09:24:42 +0300

    add list

--- src/components/elements/Messages/Messages.jsx
+++ src/components/elements/Messages/Messages.jsx
@@ -3,7 +3,13 @@ import PropTypes from 'prop-types';
 import { List } from 'semantic-ui-react';
 import Message from './Message/Message';
 
+const Messages = ({ location: { pathname }, messages }) => {
+	const id = Number(pathname.split('/')[2]);
 
+	let list;
+
+
+};
 
 export default Messages;
 
