e9657ee8651155e0b245297cee0843cffe5a275b
AntonVoronezh <pstut@mail.ru>
2019-05-23 09:19:51 +0300

    add LeftMunu

---
+++ src/components/elements/LeftMenu/LeftMenu.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Menu } from 'semantic-ui-react';
+
+const LeftMenu = props => {
+	return (
+
+	);
+};
+
+export default LeftMenu;
+
+
