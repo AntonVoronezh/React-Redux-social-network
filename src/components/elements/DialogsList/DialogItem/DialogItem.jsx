2212bb97834630a6ce7dde3b4e2538df3d2cfd81
AntonVoronezh <pstut@mail.ru>
2019-05-23 09:18:15 +0300

    add Icon

--- src/components/elements/DialogsList/DialogItem/DialogItem.jsx
+++ src/components/elements/DialogsList/DialogItem/DialogItem.jsx
@@ -5,7 +5,11 @@ import { List } from 'semantic-ui-react';
 const DialogItem = ({ id, name }) => {
 	return (
 		<List.Item>
-
+			<List.Icon name="github" size="large" verticalAlign="middle" />
+			<List.Content>
+				<List.Header >{name}</List.Header>
+				<List.Description >Updated 10 mins ago</List.Description>
+			</List.Content>
 		</List.Item>
 	);
 };
