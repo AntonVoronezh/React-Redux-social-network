d04f791f6af9bfc495fbec6ca67de2889aadd05a
AntonVoronezh <pstut@mail.ru>
2019-05-23 09:15:55 +0300

    add list

--- src/components/elements/DialogsList/DialogsList.jsx
+++ src/components/elements/DialogsList/DialogsList.jsx
@@ -5,7 +5,13 @@ import { NavLink } from 'react-router-dom';
 import DialogItem from './DialogItem/DialogItem';
 
 const Dialogs = ({ dialogs }) => {
-
+	const list = dialogs.map(d => {
+		return (
+			<NavLink key={d.id} to={`/dialogs/${d.id}`} activeStyle={{ color: 'red' }}>
+				<DialogItem {...d} />
+			</NavLink>
+		);
+	});
 
 };
 
