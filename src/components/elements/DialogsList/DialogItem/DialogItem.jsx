06ce246ae199e238b375fcdabfb62dc2b766a2e5
AntonVoronezh <pstut@mail.ru>
2019-05-23 09:17:50 +0300

    add DialogItem

---
+++ src/components/elements/DialogsList/DialogItem/DialogItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { List } from 'semantic-ui-react';
+
+const DialogItem = ({ id, name }) => {
+	return (
+		<List.Item>
+
+		</List.Item>
+	);
+};
+
+export default DialogItem;
+
