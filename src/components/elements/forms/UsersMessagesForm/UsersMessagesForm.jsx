215a863aff8d68a3bd76a264a347bd68a377caff
AntonVoronezh <pstut@mail.ru>
2019-05-25 19:57:50 +0300

    add id

--- src/components/elements/forms/UsersMessagesForm/UsersMessagesForm.jsx
+++ src/components/elements/forms/UsersMessagesForm/UsersMessagesForm.jsx
@@ -3,7 +3,13 @@ import PropTypes from 'prop-types';
 import Textarea from '../../UI/Textarea/Textarea';
 import Button from '../../UI/Button/Button';
 
+const UsersMessagesForm = ({ newDialogText, addDialogCB, addNewDialogTextCB, location: { pathname } , ...rest}) => {
+    const id = Number(pathname.split('/')[2]);
+    
+    return (
 
+	);
+};
 
 export default UsersMessagesForm;
 
