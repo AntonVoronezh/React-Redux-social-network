c00b0b5ff5a91f4d4ace947a9f32795c7e67077a
AntonVoronezh <pstut@mail.ru>
2019-05-25 19:55:37 +0300

    add User

---
+++ src/components/elements/User/User.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+import { Segment } from 'semantic-ui-react';
+import classes from './User.module.css';
+import Img from '../../../assets/img/1.png';
+import Button from '../UI/Button/Button';
+
+const Users = ({ followCB, unfollowCB, photos, name, followed, id, status }) => {
+	return (
+
+};
+
+export default Users;
+
