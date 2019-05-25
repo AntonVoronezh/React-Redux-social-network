bc77c814c51823d9fa1bd7c07cc7360c8036ada9
AntonVoronezh <pstut@mail.ru>
2019-05-25 19:57:32 +0300

    add Forms

---
+++ src/components/elements/forms/UsersMessagesForm/UsersMessagesForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Textarea from '../../UI/Textarea/Textarea';
+import Button from '../../UI/Button/Button';
+
+
+
+export default UsersMessagesForm;
+
