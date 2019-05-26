599be92ffcbb61a80ee2141e4bc13a9a71ccee73
AntonVoronezh <pstut@mail.ru>
2019-05-26 14:38:57 +0300

    ref

--- src/components/elements/UI/Button/Button.jsx
+++ src/components/elements/UI/Button/Button.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'semantic-ui-react';
 
+const ButtonMy = ({ content, callback, ...rest }) => {
+	const buttonHandler = () => {
+		callback(rest.id);
+	};
 
+};
 
 export default ButtonMy;
 
