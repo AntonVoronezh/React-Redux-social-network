f2d7c25afe46576d5bfce544996b174b99a736d0
AntonVoronezh <pstut@mail.ru>
2019-05-28 19:53:40 +0300

    add Login.jsx

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Form, Segment, Checkbox } from 'semantic-ui-react';
+import statuses from '../../../helpers/axios/constants';
 
 const Login = ({
 	addNewUsernameTextCB,
 	username,
-	password,
 	addNewPasswordTextCB,
+	password,
 	changeRememberCB,
 	remember,
-	validationFormCB,
-	loginCB
+	// validationFormCB,
+	loginCB,
+	errorMessage,
+	status
 }) => {
 	const usernameInputHandler = ({ nativeEvent: { data } }) => {
 		addNewUsernameTextCB(data);
@@ -24,7 +27,7 @@ const Login = ({
 
 	const submitFormHandler = () => {
 		// validationFormCB();
-		loginCB();
+		loginCB(username, password, remember);
 	};
 
 	return (
@@ -58,6 +61,7 @@ const Login = ({
 					Submit
 				</Button>
 			</Form>
+			{status === statuses.ERROR && <div>{errorMessage}</div>}
 		</Segment>
 	);
 };
@@ -67,9 +71,12 @@ export default Login;
 Login.propTypes = {
 	addNewUsernameTextCB: PropTypes.func.isRequired,
 	addNewPasswordTextCB: PropTypes.func.isRequired,
+	changeRememberCB: PropTypes.func.isRequired,
 	username: PropTypes.string.isRequired,
 	password: PropTypes.string.isRequired,
-	changeRememberCB: PropTypes.func.isRequired,
 	remember: PropTypes.bool.isRequired,
-	validationFormCB: PropTypes.func.isRequired,
+	loginCB: PropTypes.func.isRequired,
+	status: PropTypes.string.isRequired,
+	errorMessage: PropTypes.string,
+	// validationFormCB: PropTypes.func.isRequired,
 };
