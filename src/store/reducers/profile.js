cf0d30006c986642fa1230172c6ca6e9e4710484
AntonVoronezh <pstut@mail.ru>
2019-05-28 19:58:11 +0300

    add profile

---
+++ src/store/reducers/profile.js
@@ -0,0 +1,27 @@
+import { ADD_NEW_MESSAGE_TEXT, ADD_POST, PROFILE_REQUEST, PROFILE_SUCCESS, PROFILE_FAILURE } from '../actions/profile';
+
+const initialState = {
+	posts: [{ id: 1, message: 'aaaaa aaaaaa', likeCount: 11 }, { id: 2, message: 'bbbbbb bbbbb', likeCount: 16 }],
+	newMessageText: '---',
+	description: 'Life is life in boxing',
+	image: 'https://www.look.com.ua/large/201606/167653.jpg',
+	isLoading: false,
+	profile: null,
+	userId: null,
+};
+
+const profile = (state = initialState, action) => {
+	switch (action.type) {
+
+
+
+
+
+
+
+		default:
+			return state;
+	}
+};
+
+export default profile;
