707533539f7e85d16977e32941a34d5afe34e926
AntonVoronezh <pstut@mail.ru>
2019-05-30 20:06:21 +0300

    add auth

---
+++ src/store/actions/auth.js
@@ -0,0 +1,11 @@
+import { getMe, logOut } from '../../helpers/axios/axios';
+
+export const SET_IS_AUTH = 'SET_IS_AUTH';
+export const SET_USER = 'SET_USER';
+
+export const ME_REQUEST = 'ME_REQUEST';
+export const ME_SUCCESS = 'ME_SUCCESS';
+export const ME_FAILURE = 'ME_FAILURE';
+
+
+
