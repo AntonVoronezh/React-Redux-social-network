62f73729cf22509f6793b28c3e5a4bcc2c3fe300
AntonVoronezh <pstut@mail.ru>
2019-06-05 20:16:10 +0300

    add Dialogs

---
+++ src/containers/pages/Dialogs.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Dialogs from '../../components/pages/Dialogs/Dialogs';
+import { connect } from 'react-redux';
+import { addDialogAC, addNewDialogTextAC } from '../../store/actions/dialogs';
+
+// const get
+
+
+
