584634cce92df042c045b4edf417875614a43465
AntonVoronezh <pstut@mail.ru>
2019-06-05 20:17:56 +0300

    add Login

---
+++ src/containers/pages/Login.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Login from '../../components/pages/Login/Login';
+import { connect } from 'react-redux';
+import { addNewPasswordTextAC, addNewUsernameTextAC, changeRememberAC, login, addNewCaptchaTextAC } from '../../store/actions/login';
+
+
+
+
