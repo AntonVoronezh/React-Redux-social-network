93b5b7b200441678c39e0639c00cdfc11edc0ead
AntonVoronezh <pstut@mail.ru>
2019-06-05 20:20:51 +0300

    add Profile

---
+++ src/containers/pages/Profile.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import ProfilePage from '../../components/pages/Profile/Profile';
+import fetchProfile, { addNewMessageAC, addPostAC } from '../../store/actions/profile';
+
+class Profile extends React.Component {
+	componentDidMount() {
+		let iserId = this.props.match.params.id;
+
+		if (!iserId) {
+			iserId = 2;
+		}
+
+		this.props.fetchProfileCB(iserId);
+	}
+
+
+}
+
+
+
+
