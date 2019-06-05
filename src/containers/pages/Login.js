af29491134ce85f94ec154a1b7fc0f92160bc12d
AntonVoronezh <pstut@mail.ru>
2019-06-05 20:18:13 +0300

    add mapStateToProps

--- src/containers/pages/Login.js
+++ src/containers/pages/Login.js
@@ -3,6 +3,14 @@ import Login from '../../components/pages/Login/Login';
 import { connect } from 'react-redux';
 import { addNewPasswordTextAC, addNewUsernameTextAC, changeRememberAC, login, addNewCaptchaTextAC } from '../../store/actions/login';
 
+const mapStateToProps = ({ login, auth }) => {
+	return {
+		...login.form,
+		...login.request,
+		isAuth: auth.isAuth,
+	};
+};
+
 
 
 
