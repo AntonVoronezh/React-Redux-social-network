f8d8ad117064950919fb0fbabcc5357f02d8c98a
Anton <pstut@mail.ru>
2021-04-07 18:33:28 +0300

    add RootReducerType AppStateType

--- src/store/reducers/root.ts
+++ src/store/reducers/root.ts
@@ -1,17 +1,20 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
 
-import profile from './profile';
-import users from './users';
-import dialogs from './dialogs';
-import login from './login';
-import auth from './auth';
+import profile from "./profile";
+import users from "./users";
+import dialogs from "./dialogs";
+import login from "./login";
+import auth from "./auth";
 
 const rootReducer = combineReducers({
-	profile,
-	users,
-	dialogs,
-	login,
-	auth
+  profile,
+  users,
+  dialogs,
+  login,
+  auth,
 });
+
+export type RootReducerType = typeof rootReducer;
+export type AppStateType = ReturnType<RootReducerType>;
 
 export default rootReducer;
