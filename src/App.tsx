9926bfe1f748a41dba3bad58431c54b177ebe2e1
Anton <pstut@mail.ru>
2021-04-19 18:12:08 +0300

    add types for Profile

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import classes from "./App.module.css";
+import { Container, Menu, Segment } from "semantic-ui-react";
+import { Route, Switch } from "react-router-dom";
+
+import Main from "./containers/pages/Main";
+import Profile from "./containers/pages/Profile";
+import Dialogs from "./containers/pages/Dialogs";
+import Users from "./containers/pages/Users";
+import Login from "./containers/pages/Login";
+import NotFound from "./components/pages/NotFound/NotFound";
+import LeftMenu from "./components/elements/LeftMenu/LeftMenu";
+import Header from "./containers/elements/Header/Header";
+
+const App = () => {
+  return (
+    <div className={classes.App}>
+      <Container className={classes.container}>
+        <header className={classes.header}>
+          <Menu>
+            <Menu.Item>Social Network</Menu.Item>
+            <Menu.Item position="right">
+              <Header />
+            </Menu.Item>
+          </Menu>
+        </header>
+        <div className={classes.mainContent}>
+          <aside className={classes.aside}>
+            <Segment className={classes.segment}>
+              <LeftMenu />
+            </Segment>
+          </aside>
+          <main className={classes.main}>
+            <Segment className={classes.segment}>
+              <Switch>
+                <Route exact path="/" component={Main} />
+                <Route path="/profile/:id?" component={Profile} />
+                <Route path="/dialogs" component={Dialogs} />
+                <Route path="/dialogs/:user" component={Dialogs} />
+                <Route path="/users" component={Users} />
+                <Route path="/login" component={Login} />
+                <Route component={NotFound} />
+              </Switch>
+            </Segment>
+          </main>
+        </div>
+        <footer>
+          <Menu>
+            <Menu.Item>2019</Menu.Item>
+          </Menu>
+        </footer>
+      </Container>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/pages/Profile/Profile.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { Segment, Loader } from "semantic-ui-react";
+
+import Textarea from "../../elements/UI/Textarea/Textarea";
+import Button from "../../elements/UI/Button/Button";
+import Posts from "../../elements/Posts/Posts";
+
+type PropsType = {
+  error: { message: string };
+  posts: any;
+  newMessageText: string;
+  description: string;
+  image: any;
+  addPostCB: () => void;
+  addNewMessageCB: () => void;
+  isLoading: boolean;
+  profile: any;
+};
+
+const Profile: React.FC<PropsType> = ({
+  posts,
+  newMessageText,
+  description,
+  image,
+  addPostCB,
+  addNewMessageCB,
+  isLoading,
+  profile,
+  error,
+}) => {
+  let profileUser, result;
+
+  if (profile) {
+    !error
+      ? (profileUser = profile.fullName)
+      : (profileUser = `Ошибка "${error.message}". Попробуйте еще раз.`);
+  }
+
+  isLoading
+    ? (result = (
+        <Loader size="large" active>
+          Loading
+        </Loader>
+      ))
+    : (result = profileUser);
+
+  return (
+    <React.Fragment>
+      <Segment>
+        <img src={image} alt={description} style={{ width: "100%" }} />
+        {result}
+      </Segment>
+      <Segment>
+        <Textarea
+          newMessageText={newMessageText}
+          addNewMessageCB={addNewMessageCB}
+        />
+        <Button content="Добавить пост" callback={addPostCB} />
+      </Segment>
+      <Segment>
+        <Posts posts={posts} />
+      </Segment>
+    </React.Fragment>
+  );
+};
+
+export default Profile;
