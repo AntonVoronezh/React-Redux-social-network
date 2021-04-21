7d0e84bd591aefcd915b4da4ee8165bc5eab398d
Anton <pstut@mail.ru>
2021-04-21 18:36:39 +0300

    add types for Dialogs

---
+++ src/components/pages/Dialogs/Dialogs.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Segment } from "semantic-ui-react";
+
+import classes from "./Dialogs.module.css";
+import DialogsList from "../../elements/DialogsList/DialogsList";
+import Messages from "../../elements/Messages/Messages";
+import UsersMessagesForm from "../../elements/forms/UsersMessagesForm/UsersMessagesForm";
+
+type PropsType = {
+  newDialogText: string;
+  messages: string[];
+  dialogs: string[];
+  addDialogCB: () => void;
+  addNewDialogTextCB: () => void;
+  location: any;
+};
+
+const Dialogs: React.FC<PropsType> = (props) => {
+  return (
+    <React.Fragment>
+      <div className={classes.dialogs}>
+        <Segment className={classes.users}>
+          <DialogsList {...props} />
+        </Segment>
+        <Segment className={classes.messages}>
+          <Messages {...props} />
+          {props.location.pathname === "/dialogs" ? null : (
+            <UsersMessagesForm {...props} />
+          )}
+        </Segment>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Dialogs;
